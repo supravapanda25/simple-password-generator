5882405a5177da0580933bd357691e81e4a4ff8d
Suprava Panda <supravapanda.25@gmail.com>
2024-01-31 20:02:42 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Hey, Welcome!")
+num_letters= int(input("How many letters would you like in your password?\n")) 
+num_symbols = int(input(f"what about number of symbols?\n"))
+num_numbers = int(input(f"How many numbers would you like?\n"))
+
+password_list = []
+
+for char in range(1, num_letters + 1):
+  password_list.append(random.choice(letters))
+
+for char in range(1, num_symbols + 1):
+  password_list += random.choice(symbols)
+
+for char in range(1, num_numbers + 1):
+  password_list += random.choice(numbers)
+
+#before password shuffle-
+print(password_list)
+random.shuffle(password_list)
+
+#after password shuffle-
+print(password_list)
+
+password = ""
+for char in password_list:
+  password += char
+
+print(f"Your password is generated as: {password}")
+print("Thank you!")
